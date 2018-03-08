eb7b269dd6bd2a53af955143b45b97a9be9b9e26
Gwen Au <quyen_au@hotmail.com>
2018-03-09 08:18:14 +1100

    Default scope by event time removed for now

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,8 @@ class Event < ApplicationRecord
   geocoded_by :postcode
   after_validation :geocode   # auto-fetch coordinates - background work by Rubygem 'geocoder'
 
-  default_scope order('event_time ASC')
+  # Put in again when I can work out the syntax
+  # self.default_scope order('event_time ASC')
 
   has_and_belongs_to_many :users, :join_table => "events_users"
 end
