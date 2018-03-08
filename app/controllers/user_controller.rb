a142491f49193865e4ddadba1f7e2b26c489b09d
Gwen Au <quyen_au@hotmail.com>
2018-03-08 17:49:42 +1100

    User controller updated as per previous app

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,99 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  before_filter :authenticate_user!, except: :index
+  # GET /users
+  # GET /users.json
   def index
+    # Pagination
+    @users = User.order(:created_at).page(params[:page])
+    # Default rails index.
+    # @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
   end
 
+  # GET /users/1
+  # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    gon.selectedUser = @user 
+    
+    @upcoming_event_sample_1 = Event.all.sample
+   
+    @upcoming_event_sample_2 = Event.all.sample
+
+    if @upcoming_event_sample_1 == @upcoming_event_sample_2
+      redirect_to user_path(current_user.id) and return user_path(current_user.id)
+    end
+    
+    # binding.pry
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
   end
 
+  # GET /users/new
+  # GET /users/new.json
   def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
   end
 
+  # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+    gon.user = @user
   end
 
+  # POST /users
+  # POST /users.json
   def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PUT /users/1
+  # PUT /users/1.json
   def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /users/1
+  # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
   end
 end
+
