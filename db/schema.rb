0b5baf50ddd0c88855d19c285c2389d416dcca3e
Gwen Au <quyen_au@hotmail.com>
2018-03-08 17:26:55 +1100

    EventsUsers table created and attr_accessible error remove. More information https://stackoverflow.com/questions/25497399/rails-undefined-method-attr-accessible

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308060943) do
+ActiveRecord::Schema.define(version: 20180308062408) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
